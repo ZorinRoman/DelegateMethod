fa529b1a1f958899fe952c7a4c4165037fbd90e9
Зорин <@K218PC1>
2022-04-11 09:36:48 +0300

    Добавьте файлы проекта.

---
+++ DelegateMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMethod
+{
+    public delegate string HelloDelegate(string name);
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Hi, {s}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            HelloDelegate hD = new HelloDelegate(someClass.SayHello);
+            Console.WriteLine(hD.Invoke("Кто-то"));
+            Console.ReadLine();
+        }
+    }
+}
